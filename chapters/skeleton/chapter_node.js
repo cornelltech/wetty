ec943bbd4b0cc828e157a5e4b49625256bdcf649
Scot Spinner <sts59@cornell.edu>
2017-06-13 17:55:58 -0400

    created skeleton chapter

---
+++ chapters/skeleton/chapter_node.js
@@ -0,0 +1,78 @@
+var fs = require('fs');
+var exec = require('child_process').exec;
+module.exports = {
+  endpoints: function(app){
+  },
+  steps: [
+    { 
+      chat: "", 
+      questions: [
+        { prompt: "", 
+          answer: "" },
+        { prompt: "",
+          answer: "" },
+        { prompt: "",
+          answer: "" }
+      ], 
+      statusFunction: 
+        function(req, res) {
+        }
+    },
+    { 
+      chat: "", 
+      questions: [
+        { prompt: "", 
+          answer: "" },
+        { prompt: "",
+          answer: "" },
+        { prompt: "",
+          answer: "" }
+      ], 
+      statusFunction: 
+        function(req, res) {
+        }
+    },
+    { 
+      chat: "", 
+      questions: [
+        { prompt: "", 
+          answer: "" },
+        { prompt: "",
+          answer: "" },
+        { prompt: "",
+          answer: "" }
+      ], 
+      statusFunction: 
+        function(req, res) {
+        }
+    },
+    { 
+      chat: "", 
+      questions: [
+        { prompt: "", 
+          answer: "" },
+        { prompt: "",
+          answer: "" },
+        { prompt: "",
+          answer: "" }
+      ], 
+      statusFunction: 
+        function(req, res) {
+        }
+    },
+    { 
+      chat: "", 
+      questions: [
+        { prompt: "", 
+          answer: "" },
+        { prompt: "",
+          answer: "" },
+        { prompt: "",
+          answer: "" }
+      ], 
+      statusFunction: 
+        function(req, res) {
+        }
+    }
+  ]
+}
