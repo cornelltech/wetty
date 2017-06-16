6724efa2cf68d1124e8045f72153a94fc3ad38c7
Scot Spinner <sts59@cornell.edu>
2017-06-16 14:25:40 -0400

    adding some gifs to the end

--- chapters/quest1/chapter_frontend.js
+++ chapters/quest1/chapter_frontend.js
@@ -6,19 +6,24 @@
       });
     }
     var checkQuestStatusID;
+    var checkCountdownID;
     function checkQuestStatus() {
       $.get("/quest1/status", {}, function(data){
         if(data != 'continue'){
           if(data == 'fail'){
-            alert("Oh no!!  The bomb has gone off.  Power is being lost!");
+            clearInterval(checkCountdownID);
+            $(".status").html('<iframe src="https://giphy.com/embed/13d2jHlSlxklVe" width="480" height="318" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/reaction-explosion-government-13d2jHlSlxklVe">via GIPHY</a></p>');
             clearInterval(checkQuestStatusID);
+            theySay("Oh Nooooo!  We ran out of time!");
           } 
           if(data == 'success'){
-            alert("Yay!!  The bomb has been stopped!");
+            clearInterval(checkCountdownID);
+            $(".status").html('<iframe src="https://giphy.com/embed/eoxomXXVL2S0E" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/internet-eoxomXXVL2S0E">via GIPHY</a></p>');
             clearInterval(checkQuestStatusID);
+            theySay("Great work!!  We have saved the sector from loosing power!");
           }
         }
       });
     }
-    setInterval(checkCountdown, 1000);
+    checkCountdownID = setInterval(checkCountdown, 1000);
     checkQuestStatusID = setInterval(checkQuestStatus, 1000);
