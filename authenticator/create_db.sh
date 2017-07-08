f3229ea65d6c5bb0858badcec658198357e16fbb
Scot <scot.spinner@gmail.com>
2017-07-08 13:17:58 -0400

    updated create_db script for setup

--- authenticator/create_db.sh
+++ authenticator/create_db.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 
+# install psql locally and run this file locally.  The postgres container should be up and listening on port 5432.
 
 
 # creates table for sessions
-psql -h 127.0.0.1 -U postgres < node_modules/connect-pg-simple/table.sql
+psql -h 127.0.0.1 -U postgres wetty < node_modules/connect-pg-simple/table.sql
+psql -h 127.0.0.1 -U postgres wetty -c "CREATE TABLE users ( username varchar(40) PRIMARY KEY, data json )"
