6b54fe0ee729ee89df92d7850034eddb2edaeb63
Scot <scot.spinner@gmail.com>
2017-08-14 21:36:00 -0400

    created /api/register endpoint for signup

--- auth/app.js
+++ auth/app.js
@@ -32,8 +32,8 @@ app.set('views', '/app/auth/views/');
 
 app.get('/home', function (req, res) {
   pool.getChapters(function(data) {
-    var chapter_links = data.map(function (x) { 
-      if(req.user.available_chapters){ 
+    var chapter_links = data.map(function (x) {
+      if(req.user.available_chapters){
         return { name: x, available: req.user.available_chapters.includes(x) }
       } else {
         return { name: x, available: false }
@@ -59,7 +59,7 @@ app.post('/signup', function(req, res) {
   console.log(req.body.password);
 
   var hash = auth.createHash(req.body.password);
-  pool.addUser(req.body.username, { 
+  pool.addUser(req.body.username, {
     username: req.body.username,
     password_hash: hash,
     available_chapters: ["quest1"] });
@@ -71,6 +71,39 @@ app.post('/signup', function(req, res) {
 app.get('/login', function(req, res) {
   res.render('login');
 });
+app.post('/api/register', function(req, res){
+  console.log(req.body.username);
+  console.log(req.body.password);
+
+  var hash = auth.createHash(req.body.password);
+  pool.addUser(req.body.username, {
+    username: req.body.username,
+    password_hash: hash,
+    available_chapters: ["quest1"] });
+  docker.add_user('quest1', req.body.username, req.body.password);
+  docker.add_user('client_server', req.body.username, req.body.password);
+  docker.add_user('sample', req.body.username, req.body.password);
+  passport.authenticate('local', function(err, user, info){
+    var token;
+    // If Passport throws/catches an error
+    if (err) {
+      res.status(404).json(err);
+      return;
+    }
+    // If a user is found
+    if(user){
+      token = auth.generateJwt(user);
+      res.status(200);
+      res.json({
+        "token" : token
+      });
+    } else {
+      // If user is not found
+      res.status(401).json(info);
+    }
+
+  })(req, res);
+})
 app.get('/api/chapters', exJwt({ secret: "something_very_secret", userProperty: 'payload' }), function(req, res) {
   console.log(req.payload.user);
   pool.getChapters(function(data) {
@@ -110,11 +143,10 @@ app.get('/db', function(req, res) {
     }
     console.log(resp.rows);
     res.send(resp.rows);
-  }); 
+  });
 });
 
 
 httpserv = http.createServer(app).listen(port, function() {
   console.log('http on port ' + port);
 });
-
